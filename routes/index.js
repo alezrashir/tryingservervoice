95aef53e3756e57f72fea881d2432957fc71ca6e
shiralezra <shiralezra@gmail.com>
2018-02-25 13:08:45 +0200

    3

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,8 @@ var express = require('express');
 //var router = express.Router();
 var app = express();
 
-var busboy = require('connect-busboy');
-var result;
 
-app.use(busboy());
+
 /* GET home page. */
 app.get('/', function(req, res, next) {
   res.end('shirrrr');
@@ -16,23 +14,11 @@ app.get('/function', function(req, res, next) {
     res.end('function');
 });
 app.post('/function', function (req, res) {
+  console.log('post shir')
+res.end("post happend");
 
 
 
-    req.pipe(req.busboy);
-    req.busboy.on('file', function (fieldname, file, filename) {
-console.log("onnnnn");
-        result = filename;
-        console.log('test!!!!!!!!!!!!!!1'+filename);
-        res.end(result);
-    });
-    req.busboy.on('finish', function () {
-     //   console.log('Upload complete doneeee');
-        res.writeHead(200, { 'Connection': 'close' });
-        console.log('test!!!!!!!!!!!!!!1');
-        res.end(result);
-
-    });
 })
-;
+
         module.exports = app;
