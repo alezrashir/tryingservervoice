ed98c98c5d8d8b6e9298ca84913588c4d9d5eb48
shiralezra <shiralezra@gmail.com>
2018-02-26 08:19:51 +0200

    4

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var app = express();
 var Busboy = require('busboy');
 var multer = require('multer')
 
-//cloudconvert = new (require('cloudconvert'))('5NuswuAozpRdbnOP2xyTijteFhOOx9yZm4dkJDqMbWtLBDSQ7gIu7RofWKL5PaWg8pbB13OdpuEVOwao6EbmwQ');
+cloudconvert = new (require('cloudconvert'))('5NuswuAozpRdbnOP2xyTijteFhOOx9yZm4dkJDqMbWtLBDSQ7gIu7RofWKL5PaWg8pbB13OdpuEVOwao6EbmwQ');
 var result;
 var Client = require('ftp');
 
@@ -28,14 +28,45 @@ app.post('/function', function (req, res) {
         var c = new Client();
         c.on('ready', function() {
 
-           console.log("the file name is "+filename);
+            console.log("the file name is " + filename);
 
-                c.put(file, '/site1/'+filename, function(err) {
-                    if (err) throw err;
-                    c.end();
+            c.put(file, '/site1/input.3gp', function (err) {
+                if (err) throw err;
+
+                console.log("the input file is loaded");
+                cloudconvert.convert({
+                    inputformat: '3gp',
+                    outputformat: 'mp3',
+                    input: {
+                        ftp: {
+                            host: 'ftp-eu.site4now.net',
+                            port: '21',
+                            user: 'myfridgeapp2-001',
+                            password: 'login12345'
+                        }
+                    },
+                    file: '/site1/input.3gp',
+                    filename: 'input.3gp',
+                    output: {
+                        ftp: {
+                            host: 'ftp-eu.site4now.net',
+                            port: '21',
+                            user: 'myfridgeapp2-001',
+                            password: 'login12345',
+                            path: '/site1/output.mp3'
+                        }
+                    }
+
+                }).on('error', function (err) {
+                    console.error('Failed convert: ' + err);
+                }).on('finished', function (data) {
+                    console.log('Done convert:!!!!!!!!!!!!!!!!! ' + data.message);
+                    //  var f2 = this.pipe(fs.createWriteStream('./output.mp3'));
+
+
                 });
             });
-
+        });
         c.connect({
             host: 'ftp-eu.site4now.net',
             user: 'myfridgeapp2-001',
